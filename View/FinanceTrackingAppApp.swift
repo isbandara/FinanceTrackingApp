864f6260b5cdd018f57d60333c488fc7ead01915
Ishara Sanjeewa <gwgisanjeewabandara@gmail.com>
2023-09-24 23:28:35 -0700

    Finance tracking app create MVVM

---
+++ View/FinanceTrackingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceTrackingAppApp.swift
+//  Shared
+//
+//  Created by Ishara Sanjeewa on 2023-09-24.
+//
+
+import SwiftUI
+
+@main
+struct FinanceTrackingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ View/Login_View.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Ishara Sanjeewa on 2023-09-24.
+//
+
+import SwiftUI
+
+struct Views: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct Views_Previews: PreviewProvider {
+    static var previews: some View {
+        Views()
+    }
+}
